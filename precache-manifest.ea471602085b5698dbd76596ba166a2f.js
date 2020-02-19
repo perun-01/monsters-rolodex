63f0c66cf46205ecf6966c8851b9bad71ee8800f
perun-01 <singhranjeet5200@gmail.com>
2020-02-19 14:53:13 +0530

    Updates

---
+++ precache-manifest.ea471602085b5698dbd76596ba166a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94461f074e2c9d12cb9bc638dc1d0431",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7f3aeca7d550925a6cdd",
+    "url": "/monsters-rolodex/static/css/main.6d5007e6.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f3aeca7d550925a6cdd",
+    "url": "/monsters-rolodex/static/js/main.20476240.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
